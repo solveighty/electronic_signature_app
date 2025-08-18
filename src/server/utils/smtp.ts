2443fb5171fe0a9adbdb122741d307411b1bcb22
AndrewGlez <andrewgonzale009@gmail.com>
2025-08-18 12:55:39 -0500

    ✨ feat(smtp): Update SMTP configuration to use Hostinger settings for improved email delivery.

--- src/server/utils/smtp.ts
+++ src/server/utils/smtp.ts
@@ -1,15 +1,12 @@
 import { createTransport } from "nodemailer";
 
 const transporter = createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
   auth: {
-    type: "OAuth2",
-    user: process.env.SMTP_USER,
-    clientId: process.env.SMTP_CLIENT_ID,
-    clientSecret: process.env.SMTP_CLIENT_SECRET,
-    refreshToken: process.env.SMTP_REFRESH_TOKEN,
+    user: process.env.SMTP_USER || "",
+    pass: process.env.SMTP_PASS || "",
   },
 });
 
